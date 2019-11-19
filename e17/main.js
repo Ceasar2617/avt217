87d2ac09ad7afcc7e67c9f3475dd90929eac4f2b
Ceasar <ceasarhowe@hotmail.com>
2019-11-19 14:39:40 -0500

    e17

--- e17/main.js
+++ e17/main.js
@@ -6,26 +6,31 @@ $( document ).ready(function() {
       $('.oak').toggleClass('shrink');
       $('.oak').toggleClass('colorLightGreen');
       $('.oak h1').toggleClass('hide');
+      $('.title h1').toggleClass('font');
       });
 
     $('.maple').click(function() {
       $('.maple').toggleClass('shrink');
       $('.maple h1').toggleClass('hide');
+      $('.title h1').toggleClass('font');
       });
 
     $('.spruce').click(function() {
       $('.spruce').toggleClass('shrink');
       $('.spruce h1').toggleClass('hide');
+      $('.title h1').toggleClass('font');
     });
 
     $('.palm').click(function() {
       $('.palm').toggleClass('shrink');
       $('.palm h1').toggleClass('hide');
+      $('.title h1').toggleClass('font');
     });
 
     $('.apple').click(function() {
       $('.apple').toggleClass('shrink');
       $('.apple h1').toggleClass('hide');
+      $('.title h1').toggleClass('font');
     });
 
 
