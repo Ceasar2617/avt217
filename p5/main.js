96c63d10e85ce03df03e12fd5124eacc1ac49132
Ceasar <ceasarhowe@hotmail.com>
2019-11-26 18:38:22 -0500

    pusherino

--- p5/main.js
+++ p5/main.js
@@ -1,5 +1,12 @@
 // A $( document ).ready() block.
+//var hex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 
 function makeButton() {
-
+  var col = Math.floor(Math.random() * 10 + 1);
+  var x = Math.floor(Math.random() * 10 + 1);
+  var btn = document.createElement("Button");
+  btn.innerHTML = "Add a Button";
+  btn.setAttribute("onclick", "makeButton()");
+  btn.setAttribute("class", "color" + col);
+  document.body.appendChild(btn);
 }
